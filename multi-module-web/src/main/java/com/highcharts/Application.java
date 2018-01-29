3af1817fa4fcd3fdc185121ee074e713dc2c491e
Android-BRUCE <991027520@qq.com>
2018-01-29 09:53:19 +0800

    主pom文件加入tomcat依赖。启动类继承SpringBootServletInitializer 并且覆写SpringApplicationBuilder方法

--- multi-module-web/src/main/java/com/highcharts/Application.java
+++ multi-module-web/src/main/java/com/highcharts/Application.java
@@ -2,6 +2,8 @@ package com.highcharts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -9,7 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * 默认启动类
  */
 @SpringBootApplication()
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
